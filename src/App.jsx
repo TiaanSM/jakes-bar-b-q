d44d97426bf490c6dd18dc89f5d7f174d45e0c16
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-02-12 23:54:38 +0200

    Responsiveness and added shop page,

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Shop from './pages/Shop';
 import Menu from './pages/Menu';
 import OurRoots from './pages/OurRoots';
 import Visit from './pages/Visit';
 
 function App() {
-  
-  // button display none props selection. restaurant?
+
 
   return (
     <div className="App">
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Shop.module.css';
+
+const Shop = () => {
+  return (
+    <section className={styles.shopSection}>
+        <div className={styles.heading}>
+            <h1 className={styles.title}>WORLD-FAMOUS BBQ SAUCE</h1>
+            <h2 className={styles.desc}>Can't make it to our restaurant? Want to enjoy the love and delicious flavors of Jones Bar-B-Q in the comfort of your own home? Pick up a bottle or case of Jones Bar-B-Q Sauce.</h2>
+        </div>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Shop
